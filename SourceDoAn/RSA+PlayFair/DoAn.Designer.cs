141e7d69e7d509e0210f3100d87e3e3f1d96a3c3
Trần Kim Ngân <24521135@gm.uit.edu.vn>
2026-05-01 11:41:56 +0700

    DoAn

---
+++ SourceDoAn/RSA+PlayFair/DoAn.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace RSA_PlayFair
+{
+    partial class DoAn
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtPlayfairKey = new System.Windows.Forms.TextBox();
+            this.txtPlayfairInput = new System.Windows.Forms.TextBox();
+            this.txtPlayfairResult = new System.Windows.Forms.TextBox();
+            this.btnPlayfairEncrypt = new System.Windows.Forms.Button();
+            this.txtRSAP = new System.Windows.Forms.TextBox();
+            this.txtRSAQ = new System.Windows.Forms.TextBox();
+            this.txtRSAInput = new System.Windows.Forms.TextBox();
+            this.txtRSAResult = new System.Windows.Forms.TextBox();
+            this.btnRSAEncrypt = new System.Windows.Forms.Button();
+            this.lblRSAKeys = new System.Windows.Forms.Label();
+
+         
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Size = new System.Drawing.Size(434, 461);
+
+            
+            this.tabPage1.Text = "Giải thuật Playfair";
+            this.txtPlayfairKey.PlaceholderText = "Nhập từ khóa (Key)...";
+            this.txtPlayfairKey.Location = new System.Drawing.Point(30, 30);
+            this.txtPlayfairKey.Size = new System.Drawing.Size(350, 27);
+
+            this.btnPlayfairEncrypt.Text = "Mã hóa văn bản";
+            this.btnPlayfairEncrypt.Location = new System.Drawing.Point(30, 70);
+            this.btnPlayfairEncrypt.Size = new System.Drawing.Size(150, 35);
+            this.btnPlayfairEncrypt.BackColor = System.Drawing.Color.LightBlue;
+            this.btnPlayfairEncrypt.Click += new System.EventHandler(this.btnPlayfairEncrypt_Click);
+
+            this.txtPlayfairInput.PlaceholderText = "Nhập văn bản cần mã hóa...";
+            this.txtPlayfairInput.Location = new System.Drawing.Point(30, 120);
+            this.txtPlayfairInput.Multiline = true;
+            this.txtPlayfairInput.Size = new System.Drawing.Size(350, 80);
+
+            this.txtPlayfairResult.PlaceholderText = "Kết quả Playfair sẽ hiện ở đây...";
+            this.txtPlayfairResult.Location = new System.Drawing.Point(30, 220);
+            this.txtPlayfairResult.Multiline = true;
+            this.txtPlayfairResult.Size = new System.Drawing.Size(350, 80);
+            this.txtPlayfairResult.ReadOnly = true;
+
+           
+            this.tabPage2.Text = "Giải thuật RSA";
+            this.txtRSAP.PlaceholderText = "Số p";
+            this.txtRSAP.Location = new System.Drawing.Point(30, 30);
+            this.txtRSAP.Size = new System.Drawing.Size(160, 27);
+
+            this.txtRSAQ.PlaceholderText = "Số q";
+            this.txtRSAQ.Location = new System.Drawing.Point(220, 30);
+            this.txtRSAQ.Size = new System.Drawing.Size(160, 27);
+
+            this.btnRSAEncrypt.Text = "Thực hiện RSA";
+            this.btnRSAEncrypt.Location = new System.Drawing.Point(30, 70);
+            this.btnRSAEncrypt.Size = new System.Drawing.Size(150, 35);
+            this.btnRSAEncrypt.BackColor = System.Drawing.Color.LightGreen;
+            this.btnRSAEncrypt.Click += new System.EventHandler(this.btnRSAEncrypt_Click);
+
+            this.txtRSAInput.PlaceholderText = "Nhập thông điệp (dạng số)...";
+            this.txtRSAInput.Location = new System.Drawing.Point(30, 120);
+            this.txtRSAInput.Size = new System.Drawing.Size(350, 27);
+
+            this.txtRSAResult.PlaceholderText = "Kết quả mã hóa RSA...";
+            this.txtRSAResult.Location = new System.Drawing.Point(30, 160);
+            this.txtRSAResult.Size = new System.Drawing.Size(350, 27);
+            this.txtRSAResult.ReadOnly = true;
+
+            this.lblRSAKeys.Location = new System.Drawing.Point(30, 200);
+            this.lblRSAKeys.Size = new System.Drawing.Size(350, 100);
+            this.lblRSAKeys.Text = "Thông tin Key: Đợi tính toán...";
+
+           
+            this.tabPage1.Controls.AddRange(new System.Windows.Forms.Control[] { txtPlayfairKey, btnPlayfairEncrypt, txtPlayfairInput, txtPlayfairResult });
+            this.tabPage2.Controls.AddRange(new System.Windows.Forms.Control[] { txtRSAP, txtRSAQ, btnRSAEncrypt, txtRSAInput, txtRSAResult, lblRSAKeys });
+            this.tabControl1.TabPages.AddRange(new System.Windows.Forms.TabPage[] { tabPage1, tabPage2 });
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 461);
+            this.Controls.Add(this.tabControl1);
+            this.Text = "Đồ án UIT - An toàn mạng";
+        }
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtPlayfairKey;
+        private System.Windows.Forms.TextBox txtPlayfairInput;
+        private System.Windows.Forms.TextBox txtPlayfairResult;
+        private System.Windows.Forms.Button btnPlayfairEncrypt;
+        private System.Windows.Forms.TextBox txtRSAP;
+        private System.Windows.Forms.TextBox txtRSAQ;
+        private System.Windows.Forms.TextBox txtRSAInput;
+        private System.Windows.Forms.TextBox txtRSAResult;
+        private System.Windows.Forms.Button btnRSAEncrypt;
+        private System.Windows.Forms.Label lblRSAKeys;
+    }
+}
---
+++ SourceDoAn/RSA+PlayFair/DoAn.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace RSA_PlayFair
+{
+    public partial class DoAn : Form
+    {
+        public DoAn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlayfairEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string key = txtPlayfairKey.Text.ToUpper().Replace("J", "I");
+                string input = txtPlayfairInput.Text.ToUpper().Replace("J", "I").Replace(" ", "");
+                if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(input)) return;
+
+                char[,] matrix = GenerateMatrix(key);
+                txtPlayfairResult.Text = ProcessPlayfair(matrix, input);
+            }
+            catch (Exception ex) { MessageBox.Show("Lỗi: " + ex.Message); }
+        }
+
+        private char[,] GenerateMatrix(string key)
+        {
+            string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+            string combined = new string((key + alphabet).Distinct().ToArray());
+            char[,] m = new char[5, 5];
+            for (int i = 0; i < 25; i++) m[i / 5, i % 5] = combined[i];
+            return m;
+        }
+
+        private string ProcessPlayfair(char[,] m, string input)
+        {
+           
+            string result = "";
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                char a = input[i];
+                char b = (i + 1 < input.Length) ? input[i + 1] : 'X';
+                if (a == b) { b = 'X'; i--; }
+
+                Point p1 = GetPos(m, a);
+                Point p2 = GetPos(m, b);
+                if (p1.X == p2.X) result += m[p1.X, (p1.Y + 1) % 5].ToString() + m[p2.X, (p2.Y + 1) % 5].ToString();
+                else if (p1.Y == p2.Y) result += m[(p1.X + 1) % 5, p1.Y].ToString() + m[(p2.X + 1) % 5, p2.Y].ToString();
+                else result += m[p1.X, p2.Y].ToString() + m[p2.X, p1.Y].ToString();
+            }
+            return result;
+        }
+
+        private Point GetPos(char[,] m, char c)
+        {
+            for (int r = 0; r < 5; r++)
+                for (int col = 0; col < 5; col++)
+                    if (m[r, col] == c) return new Point(r, col);
+            return new Point(-1, -1);
+        }
+
+        private void btnRSAEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BigInteger p = BigInteger.Parse(txtRSAP.Text);
+                BigInteger q = BigInteger.Parse(txtRSAQ.Text);
+                BigInteger n = p * q;
+                BigInteger phi = (p - 1) * (q - 1);
+                BigInteger ev = 65537;
+                BigInteger d = ModInverse(ev, phi);
+                BigInteger m = BigInteger.Parse(txtRSAInput.Text);
+
+                txtRSAResult.Text = BigInteger.ModPow(m, ev, n).ToString();
+                lblRSAKeys.Text = $"N: {n}\nE (Công khai): {ev}\nD (Bí mật): {d}";
+            }
+            catch { MessageBox.Show("Vui lòng nhập các số nguyên hợp lệ!"); }
+        }
+
+        public static BigInteger ModInverse(BigInteger a, BigInteger n)
+        {
+            BigInteger t = 0, nt = 1, r = n, nr = a;
+            while (nr != 0)
+            {
+                BigInteger q = r / nr;
+                (t, nt) = (nt, t - q * nt);
+                (r, nr) = (nr, r - q * nr);
+            }
+            return t < 0 ? t + n : t;
+        }
+    }
+}
